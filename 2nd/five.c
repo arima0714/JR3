be5797b3a5b4d1d61555876a724435fa31415a79
@rima <a1710021@edu.cc.uec.ac.jp>
2018-10-14 09:30:24 +0900

    5に再挑戦

---
+++ 2nd/five.c
@@ -0,0 +1,145 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <unistd.h>
+// #include <string.h>
+
+// #define MAXLEN 128
+// #define MAXROW 64
+
+// char buf[MAXLEN+2];
+// int numbers[MAXROW][MAXLEN];
+
+// void *
+// xmaloc(size_t size){
+// 	void *p = malloc(size);
+// 	return p;
+// }
+
+// int main(){
+
+// 	char *p;
+// 	int j = 0;
+// 	int k = 0;
+// 	int row;
+// 	// int colum;
+// 	int sa = 0;
+
+// 	while(fgets(buf,sizeof(buf),stdin) != NULL){
+// 		p = strchr(buf,'\n');
+// 		if(p != NULL) *p = '\0';
+// 		printf("%s\n",buf);
+
+// 		p = buf;
+// 		int i;
+// 		while(sscanf(p,"%d",&i) != EOF){
+// 			printf("%d\n",i);
+// 			numbers[j][k] = i;
+// 			p = strchr(p,' ');
+// 			if(p == NULL) break;
+// 			else while(*p == ' ') ++p;
+// 			k++;
+// 		}
+// 		numbers[j][k] = -1;
+// 		j++;
+// 	}
+
+// 	row = j;
+
+// 	printf("row = %d",row);
+
+// 	for(int i = 0; i < row; i++){
+// 		for(j = 0; ;j++){
+// 			if(numbers[i][j] < 0){
+// 				break;
+// 			}else{
+// 				// printf("numbers[%d][%d] = %d\n",i,j,numbers[i][j]);
+
+// 			}
+// 		}
+// 	}
+// 	// p = buf;
+// 	// int i;
+// 	// while(sscanf(p,"%d",&i) != EOF){
+// 	// 	printf("%d\n",i);
+// 	// 	p = strchr(p,' ');
+// 	// 	if(p == NULL) break;
+// 	// 	else while(*p == ' ') ++p;
+// 	// }
+
+// }
+
+// 元の状態　-2
+// 何か追加されたら　＃
+// 空白　-1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXLEN 128
+#define MAXROW 64
+
+char buf[MAXLEN+2];
+int numbers[MAXROW][MAXLEN];
+
+void *
+xmaloc(size_t size){
+	void *p = malloc(size);
+	return p;
+}
+
+int main(){
+
+	char *p;
+	int j = 0;
+	// int k = 0;
+	int row;
+	// int sa = 0;
+
+	for(int row = 0;row < MAXROW;row++){
+		for(int colum = 0;colum < MAXLEN;colum++){
+			numbers[row][colum] = -2;
+		}
+	}
+
+	while(fgets(buf,sizeof(buf),stdin) != NULL){
+		p = strchr(buf,'\n');
+		if(p != NULL) *p = '\0';
+		// printf("%s\n",buf);
+
+		p = buf;
+		int i;
+		// int len = 0;
+		while(sscanf(p,"%d",&i) != EOF){
+			// printf("%d\n",i);
+			numbers[j][i] = i;
+			p = strchr(p,' ');
+			if(p == NULL) break;
+			else while(*p == ' ') ++p;
+		}
+		numbers[j][i+1] = -1;
+		j++;
+	}
+
+	row = j;
+
+	// printf("row = %d\n",row);
+
+	for(int i = 0; i < row; i++){
+		for(j = 0; ;j++){
+			if(numbers[i][j] == -1){
+				printf("\n");
+				break;
+			}else{
+				// printf("numbers[%d][%d] = %d\n",i,j,numbers[i][j]);
+				if(numbers[i][j] < 0){
+					printf("\0");
+				}else{
+					printf("#");
+				}
+			}
+		}
+	}
+
+}
--- 2nd/four.c
+++ 2nd/four.c
@@ -8,12 +8,45 @@
 int main(){
 
     int i;
-    int fib[100001] = {0,1};
-    for (i = 2; i < 100001;++i){
-        fib[i] = fib[i - 1] + fib[i - 2];
+    int j;
+    int num_of_strings;
+    int prime_numbers[1001];
+    int prime[11] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 };
+    for (i = 0; i < 1001; i++) {
+        prime_numbers[i] = i;
+    }
+    for (j = 0; j < 11; j++){
+        for (i = 0; i <= 1001; i++){
+            if(i % prime[j] == 0 && prime[j] != i){
+                prime_numbers[i] = -1;
+            }else if(prime_numbers[j] == 1){
+                prime_numbers[i] = -1;
+            }
+        }
     }
 
-	char str[1000];
-	fgets(str,1000,stdin);
-	printf("%s",str);
+    char str[1000];
+    char* bf;
+    bf = str;
+    fgets(str, 1000, stdin);
+
+    for(num_of_strings = 0; str[num_of_strings] != '\0'; ++num_of_strings){
+    }
+    num_of_strings--;
+
+    // for(i = 0 ; i < num_of_strings; i++){
+    //     if(prime_numbers[i] != -1){
+    //         printf("%c", str[prime_numbers[i-1]]);
+    //     }
+    // }
+
+    for(i = 0; i <= num_of_strings; i++){
+        if(prime_numbers[i] != -1){
+            printf("%c",str[prime_numbers[i]-1]);
+        }
+    }
+
+
+
+    printf("\n");
 }
