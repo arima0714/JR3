08ae6767256298ac0e73b4bbc8d2cbba2b42c3b4
@rima <a1710021@edu.cc.uec.ac.jp>
2018-10-22 14:09:19 +0900

    4のとちゅうです

---
+++ 3rd/4four.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+int number = 0;
+
+struct golden{
+	long long int a;
+	long long int b;
+};
+
+struct golden double_golden(struct golden x){
+	struct golden y;
+	y.a = x.a * 2;
+	y.b = x.b * 2;	
+	return y; 
+}
+
+struct golden add_golden(struct golden x, struct golden y){
+	struct golden z;
+	z.a = x.a + y.a;
+	z.b = x.b + y.b;
+	return z;
+}
+
+struct golden mult_golden(struct golden x, struct golden y){
+	number += 1;
+	struct golden z;
+	z.a = x.a * y.a + x.b * y.b;
+	z.b = x.a * y.b + y.a * x.b + x.b * y.b;
+	return z;
+}
+
+struct golden power_golden(struct golden x,int n){
+	struct golden z;
+
+	return z;
+}
+
+int main(){
+	struct golden x;
+	int n;
+	scanf("%lld %lld",&x.a,&x.b);
+	scanf("%d",&n);	
+	x = power_golden(x,n);
+	printf("%lld %lld\n",x.a, x.b);
+	printf("%d\n",number);
+	return 0;
+}
