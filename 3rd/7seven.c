6eff620f04d13ad2b3af6dc9c6038e815c864919
@rima <a1710021@edu.cc.uec.ac.jp>
2018-10-29 22:43:09 +0900

    とりあえず提出はできました

--- 3rd/7seven.c
+++ 3rd/7seven.c
@@ -10,55 +10,30 @@ struct golden{
 	long long int b;
 };
 
-// struct golden double_golden(struct golden x){
-// 	struct golden y;
-// 	y.a = x.a * 2;
-// 	y.b = x.b * 2;	
-// 	return y; 
-// }
-
-// struct golden add_golden(struct golden x, struct golden y){
-// 	struct golden z;
-// 	z.a = x.a + y.a;
-// 	z.b = x.b + y.b;
-// 	return z;
-// }
-
-struct golden mult_golden(struct golden x, struct golden y){
-	struct golden z;
-	z.a = x.a * y.a + x.b * y.b;
-	z.b = x.a * y.b + y.a * x.b + x.b * y.b;
-	add_number += 3;
-	mult_number += 5;
-	return z;
-}
-
-struct golden power_golden(struct golden x,int n){
-	struct golden z;
-	if(n == 0){
-		z.a = 1;
-		z.b = 0;
-	}else if(n%2 == 1){
-		z = mult_golden(x,power_golden(x,n-1));
-	}else if(n%2 == 0){
-		struct golden y;
-		y = power_golden(x,n/2);
-		z = mult_golden(y,y);
-
-	}
-	return z;
-}
-
 int size_of_golden(struct golden x, struct golden y){
-    float root_five = pow(5,0.5);
-    float phi = (1+root_five)/2;
-    if(x.a + x.b * phi > y.a + y.b * phi){
-	return 1;
-    }else if(x.a + x.b * phi == y.a + y.b * phi){
-	return 0;
-    }else{
-	return -1;
-    }
+	long long int R;
+	long long int I;
+	
+	if(x.a == y.a && x.b == y.b){return 0;}
+
+	R = 2 * x.a - 2 * y.a + x.b - y.b;
+	I = x.b - y.b;
+
+	if(R > 0 && I >0){return 1;}
+	else if(R < 0 && I < 0){return -1;}
+	else if(R > 0 && I < 0){
+		if(R*R - 5*I*I > 0){
+			return 1;					
+		}else{
+			return -1;
+		}
+	}else if(R < 0 && I > 0){
+		if(R * R - 5 * I * I < 0){
+			return -1;
+		}else{
+			return 1;
+		}
+	}
 }
 
 int main(){
@@ -69,4 +44,6 @@ int main(){
     scanf("%lld%lld",&two.a,&two.b);
 	
     printf("%d\n",size_of_golden(one, two));
+
+    return 0;
 }
--- 3rd/8eight.c
+++ 3rd/8eight.c
@@ -2,33 +2,35 @@
 #include <string.h>
 #include <math.h>
 
-int fib_first = 0;
-int fib_second = 1;
-int fib_third = 1;
+long long int fib_first = 0;
+long long int fib_second = 1;
+long long int fib_third = 1;
 
-int return_divided(int n){
-	int fib_temp_one = 0;
-	int fib_temp_two = 0;
-	for(int i = 0;i < n;i++){
-		if(n == 0){
-			return fib_first;
-		}else if(n == 1){
-			return fib_second;
-		}else if(n == i){
-			return fib_third;
-		}else{
-			fib_temp_one = fib_first;
-			fib_temp_two = fib_second;
-			fib_first = fib_second;
-			fib_second = fib_third;
-			fib_third = fib_temp_one + fib_temp_two;	
-		}
+long long int return_divided(int n){
+    long long int a = 0,b = 1,c,i;
+    for(i = 0;i <= n; ++i){
+	if(i == 0){
+		//printf("i = %lld, fib = %lld\n",i,a);
+	}else if(i == 1){
+		//printf("i = %lld, fib = %lld\n",i,b);
+	//}else if(n == i){
+	//	return c;
+
+	}else{
+		c = a%2018 + b%2018;
+		c = c%2018;
+		//printf("i = %lld , fib = %lld\n",i,c);
+		a = b;
+		b = c;
+		//printf("i = %d, c = %d\n",i,c);
 	}
+    }
+    return c;
 }
 
 int main(void){
 	int n;
 	scanf("%d",&n);
-	printf("n = %d, fib = %d",n,return_divided(n));
+	printf("%lld\n",return_divided(n));
 	return 0;
 }
