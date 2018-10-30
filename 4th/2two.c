ac6bb900c1227e37f3662a8cdf23b4354653973a
K_Arima <a1710021@edu.cc.uec.ac.jp>
2018-10-30 15:27:30 +0900

    全角スペースのエラー

--- 4th/2two.c
+++ 4th/2two.c
@@ -7,7 +7,18 @@ struct zahyo{
 };
 
 void update(struct zahyo *p,int i,int j){
+	(*p).x += i;
+	(*p).y += j;
+	int x = (*p).x;
+	int y = (*p).y;
+	x = x * x;
+	y = y * y;
+ 	if(x + y>= 100 * 100){
+		(*p).x = 0;
+		(*p).y = 0;
+	}
 
+	
 	return ;
 
 }
--- 4th/3three.c
+++ 4th/3three.c
@@ -1,11 +1,60 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 #define MAXSTACK 128;	
-typedef char elementtype;
+
+typedef int elementtype;
 
 struct stack{
 	int top;
 	elementtype contents[MAXSTACK];
 };
 
+void initstack(struct stack *p){
+	(*p).top = 0;
+}
 
+
+int stackempty(struct stack *p){
+	if((*p).top == 0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+elementtype pop(struct stack *p){
+
+}
+
+void push(struct stack *p, elementtype e){
+	if(p->top == MAXSTACK){
+		printf("Overflow\n");
+	}else{
+	
+	}
+}
+
+void print_stacks(*p){
+	int i;
+	for(i = 0;i < p->top;i++){
+		printf("[%d]",p->contents[i]);		
+	}
+	printf("\n");
+}
+
+
+int main(){
+	struct stack s;
+	int i;
+	/* スタック s の初期化 */
+	while(fgets(buf,sizeof(buf),stdin) != NULL) {
+		if(buf[0]==’p’) {
+		/* スタック s からのポップ操作 */
+		} else {
+		sscanf(buf,"%d", &i);
+		/* スタック s への i のプッシュ操作 */
+		}
+	/* スタック s の中身を指定された形式で出力 */
+	}
+}
