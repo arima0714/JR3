a865b042edbc591021e48307f304648f157ddbfd
@rima <a1710021@edu.cc.uec.ac.jp>
2018-11-02 19:25:36 +0900

    6ができました

--- 4th/6six.c
+++ 4th/6six.c
@@ -1,10 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #define MAXQUEUE 128 
 
 char buf[MAXQUEUE];
-
 typedef int elementtype;
 
 struct queue{
@@ -29,16 +27,37 @@ int queueempty(struct queue *p){
 	}
 }
 
+void print_queue(struct queue *p){
+	for(int i = p->front; i < p->rear; i++){
+		printf("[%d]",p->contents[i]);
+	}
+	printf("\n");
+}
+
 elementtype getq(struct queue *p){
 // 構造体 queue のアドレスを受け取り，そのキューから要素を取り出し，その要素を返す関数．
-
+	if(queueempty(p) == 0){
+		printf("Underflow\n");
+		exit(1);
+	}else{
+		elementtype content = p->contents[p->front];
+		p->front++;
+		print_queue(p);
+		return content;
+	}
 }
 
 void putq(struct queue *p, elementtype e){
 // 構造体 queue のアドレスを受け取り，そのキューに e を入れる関数．
-
+	if(p->rear == MAXQUEUE){
+		printf("Sorry\n");
+		exit(1);
+	}else{
+		p->contents[p->rear] = e;
+		p->rear++;
+		print_queue(p);
+	}	
 }
-
 
 int main(){
 	struct queue s;
