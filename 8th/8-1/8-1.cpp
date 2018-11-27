27196cdec6c864cde908fd24815bb29210a28feb
@rima <a1710021@edu.cc.uec.ac.jp>
2018-11-27 15:14:32 +0900

    8-1ができたっぽい？

---
+++ 8th/8-1/8-1.cpp
@@ -0,0 +1,137 @@
+﻿// 8-1.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+char kijun;
+
+struct point {
+	int x;
+	int y;
+};
+
+int compare_by(struct point p1, struct point p2) {
+	//ここを埋める
+	if (p1.x == p2.x && p1.y == p2.y) {
+		return 0;
+	}
+	else if (kijun == 'X') {//Xの処理
+		if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else if (kijun == 'Y') {//Yの処理
+		if (p1.y > p2.y) {
+			return 1;
+		}
+		else if (p1.y < p2.y) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {//Dの処理
+		int p1xy = p1.x * p1.x + p1.y * p1.y;
+		int p2xy = p2.x * p2.x + p2.y * p2.y;
+		if (p1xy > p2xy) {
+			return 1;
+		}
+		else if (p1xy < p2xy) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+int partition(struct point a[], int m, int n) {
+	struct point pivot;
+	struct point temp;
+	struct point temp_one;
+	struct point temp_two;
+	int l;
+	int r;
+	pivot = a[m];
+	l = m + 1;
+	r = n;
+	while (1) {
+		while (l < n && compare_by(a[l], pivot) == -1) {
+			l++;
+		}
+		while (r > m && compare_by(a[r], pivot) == 1) {
+			r--;
+		}
+		if (l < r) {
+			temp = a[l];
+			a[l] = a[r];
+			a[r] = temp;
+		}
+		else {
+			break;
+		}
+		l++;
+		r--;
+	}
+	temp_one = a[r];
+	temp_two = pivot;
+	a[m] = temp_one;
+	a[r] = temp_two;
+	return r;
+}
+
+int main()
+{
+	char buf[128];
+	struct point p;
+	struct point arr[128];
+	int i = 0;
+	int n;
+	scanf("%c ", &kijun);
+	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	n = i;
+	printf("%d\n", partition(arr, 0, n - 2));
+	for (i = 0;i < n;++i) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+	return 0;
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
