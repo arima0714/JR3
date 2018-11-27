84e89c7064e8ae917265a35a1893bf63b19186d7
omame1234 <a1710021@edu.cc.uec.ac.jp>
2018-11-27 23:09:26 +0900

    謎のsegmentation fault

---
+++ 7th/7-4/7-4.c
@@ -0,0 +1,101 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "pch.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+struct point {
+	int x;
+	int y;
+};
+
+int compare_by(struct point p1, struct point p2, char c) {
+	//ここを埋める
+	if (p1.x == p2.x && p1.y == p2.y) {
+		return 0;
+	}
+	else if (c == 'X') {//Xの処理
+		if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else if (c == 'Y') {//Yの処理
+		if (p1.y > p2.y) {
+			return 1;
+		}
+		else if (p1.y < p2.y) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {//Dの処理
+		int p1xy = p1.x * p1.x + p1.y * p1.y;
+		int p2xy = p2.x * p2.x + p2.y * p2.y;
+		if (p1xy > p2xy) {
+			return 1;
+		}
+		else if (p1xy < p2xy) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+void insert_by(struct point a[], int n, char c) {
+	//以下を埋める
+	int i = n-1;
+	struct point temp;
+	while (compare_by(a[i-1], a[i], c) == 1) {
+		temp = a[i];
+		a[i] = a[i - 1];
+		a[i - 1] = temp;
+		i--;
+	}
+}
+
+int main() {
+	char c;
+	char buf[128];
+	struct point p;
+	struct point arr[128];
+	int i = 0;
+	int n;
+	scanf("%c ", &c);
+	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	n = i;
+	insert_by(arr, n, c);
+	for (i = 0;i < n;++i) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+	return 0;
+}
