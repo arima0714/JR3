0a6813770a50991adb08177cf561e298e60ad158
@rima <a1710021@edu.cc.uec.ac.jp>
2018-11-28 19:42:57 +0900

    7-6の下書きが完了しました

--- 7th/7-6/7-6.cpp
+++ 7th/7-6/7-6.cpp
@@ -4,12 +4,15 @@
 
 #include <stdio.h>
 
+int count = 0;
+
 struct point {
 	int x;
 	int y;
 };
 
 int compare_by(struct point p1, struct point p2, char c) {
+	count++;
 	//ここを埋める
 	if (p1.x == p2.x && p1.y == p2.y) {
 		return 0;
@@ -85,22 +88,42 @@ int max_index_by(struct point a[], int n, char c) {
 	return max_i;
 }
 
+void bubble_sort(struct point a[], int n, char c) {
+	struct point tmp;
+	int result;
+	for (int k = n - 2;k >= 0;k--) {
+		for (int l = 0;l <= k;l++) {
+			struct point a_l = a[l];
+			struct point a_l_plus_one = a[l + 1];
+			result = compare_by(a[l], a[l + 1], c);
+			if (result != -1) {
+				tmp = a[l];
+				a[l] = a[l + 1];
+				a[l + 1] = tmp;
+			}
+		}
+	}
+}
+
 int main()
 {
-	//printf("hello wordd\n");
-
 	char c;
 	char buf[128];
 	struct point p;
 	struct point arr[128];
 	int i = 0;
+	int n;
 	scanf("%c ", &c);
 	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
 		sscanf(buf, "%d %d", &p.x, &p.y);
 		arr[i] = p;
 		++i;
 	}
-	printf("%d\n", max_index_by(arr, i, c));
-
+	n = i;
+	bubble_sort(arr, n, c);
+	printf("%d\n", count);
+	for (i = 0;i < n;++i) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
 	return 0;
 }
