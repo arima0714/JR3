9b9487c8d81720428f48cfb4936043d61274af83
@rima <a1710021@edu.cc.uec.ac.jp>
2018-12-04 20:34:04 +0900

    4したがき

--- 9th/4/4.cpp
+++ 9th/4/4.cpp
@@ -10,7 +10,30 @@ struct point {
 };
 
 void bucket_sort(struct point a[], int n) {
-
+	//もともとの順番を崩さないようにする
+	//並び替え方法は x の値で昇順
+	// 0 <= x <= 99 の範囲で x が与えられる
+	int i;
+	//9ページ目の(1)
+	int hairetu_c[100] = {};
+	//10ページ目の(2)
+	for (i = 0;i < n;i++) {
+		hairetu_c[a[i].x]++;
+	}
+	//10ページ目の(3)
+	for (i = 1;i < 100;i++) {
+		hairetu_c[i] += hairetu_c[i - 1];
+	}
+	//10ページ目の(4)
+	struct point hairetu_b[128];
+	for (i = 0;i < 128;i++) {
+		hairetu_b[i] = a[i];
+	}
+	//10ページ目の(5)
+	for (i = 127;i >= 0;--i) {
+		hairetu_c[hairetu_b[i].x]--;
+		a[hairetu_c[i]] = hairetu_b[i];
+	}
 }
 
 int main() {
