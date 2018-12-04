f2712be7a26387a36b8fff4dace20d91389c9f99
@rima <a1710021@edu.cc.uec.ac.jp>
2018-12-04 19:45:05 +0900

    　

--- 9th/4/4.cpp
+++ 9th/4/4.cpp
@@ -1,5 +1,33 @@
 ﻿#include "pch.h"
-#define _CRT_SECURE_NO_WARNIGNS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+struct point {
+	int x;
+	int y;
+};
+
+void bucket_sort(struct point a[], int n) {
+
+}
+
+int main() {
+	char buf[128];
+	struct point p;
+	struct point arr[128];
+	int i = 0;
+	int n;
+	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	n = i;
+	bucket_sort(arr, n);
+	for (i = 0;i < n;++i) {
+		printf("%d %d\n", arr[i].x, arr[i].y);
+	}
+	return 0;
+}
