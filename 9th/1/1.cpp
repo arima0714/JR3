f5a899368a25290eedce0af1047e7256db6d744c
@rima <a1710021@edu.cc.uec.ac.jp>
2018-12-04 14:56:32 +0900

    9-1下書き

--- 9th/1/1.cpp
+++ 9th/1/1.cpp
@@ -1,5 +1,103 @@
 ﻿#include "pch.h"
-#define _CRT_SECURE_NO_WARNIGNS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+char kijun;
+struct point {
+	int x;
+	int y;
+};
+
+int compare_by(struct point p1, struct point p2) {
+	//ここを埋める
+	if (p1.x == p2.x && p1.y == p2.y) {
+		return 0;
+	}
+	else if (kijun == 'X') {//Xの処理
+		if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else if (kijun == 'Y') {//Yの処理
+		if (p1.y > p2.y) {
+			return 1;
+		}
+		else if (p1.y < p2.y) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {//Dの処理
+		int p1xy = p1.x * p1.x + p1.y * p1.y;
+		int p2xy = p2.x * p2.x + p2.y * p2.y;
+		if (p1xy > p2xy) {
+			return 1;
+		}
+		else if (p1xy < p2xy) {
+			return -1;
+		}
+		else if (p1.x > p2.x) {
+			return 1;
+		}
+		else if (p1.x < p2.x) {
+			return -1;
+		}
+		else if (p1.y > p2.y) {
+			return 1;
+		}
+		else {
+			return -1;
+		}
+	}
+}
+
+int is_heap(struct point a[], int n) {
+	int i = 0;
+	for (i = 0;2 * i + 1 < n;i++) {
+		if (compare_by(a[2 * i + 1], a[i]) == 1) {
+			return false;
+		}
+		if (compare_by(a[2 * i + 2], a[i]) == 1) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main() {
+	char buf[128];
+	struct point p;
+	struct point arr[128];
+	int i = 0;
+	int n;
+	scanf("%c ", &kijun);
+	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	n = i;
+	if (is_heap(arr, n)) {
+		printf("Yes.\n");
+	}
+	else {
+		printf("No.\n");
+	}
+	return 0;
+}
