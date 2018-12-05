67902ee77c049baa71283132286946194397dc8a
@rima <a1710021@edu.cc.uec.ac.jp>
2018-12-06 08:59:53 +0900

    下書き中

--- 9th/5/5.cpp
+++ 9th/5/5.cpp
@@ -1,5 +1,34 @@
 ﻿#include "pch.h"
-#define _CRT_SECURE_NO_WARNIGNS
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+
+struct point {
+	int x;
+	int y;
+};
+
+void radix_sort(struct point a[], int n, int r, int dmax) {
+
+}
+
+int main() {
+	char buf[128];
+	struct point p;
+	struct point arr[128];
+	int i = 0;
+	int n;
+	int r;
+	int dmax;
+	scanf("%d %d ", &r, &dmax);
+
+	while (fgets(buf, sizeof(buf), stdin) != NULL && i < 128) {
+		sscanf(buf, "%d %d", &p.x, &p.y);
+		arr[i] = p;
+		++i;
+	}
+	n = i;
+	radix_sort(arr, n, r, dmax);
+	return 0;
+}
