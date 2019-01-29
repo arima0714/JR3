7ef5849c5abb5e5dd374009a427f56c84b9a5a2f
@rima <a1710021@edu.cc.uec.ac.jp>
2019-01-29 15:25:52 +0900

    反例修正反例修正

--- 15th/1/1.c
+++ 15th/1/1.c
@@ -3,7 +3,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define SETMAX 10600
 
-int main(){
-    printf("hello JR3\n");
+struct set {
+    int elements[SETMAX];
+    int size;
+};
+
+void init_set(struct set* p, int n, int e)
+{
+    int j= 0;
+    int i= 0;
+    p->size = n - 1;
+    while(i < n){
+        if(i != e){
+            p->elements[j] = i;
+            j++;
+        }
+        i++;
+    }
+}
+
+void print_set(struct set* p)
+{
+    int i;
+    printf("{");
+    for (i = 0; i < p->size; ++i) {
+        printf(" %d", p->elements[i]);
+    }
+    printf(" }\n");
+}
+
+int main()
+{
+    int n;
+    int e;
+    struct set s;
+    scanf("%d %d ", &n, &e);
+    init_set(&s, n, e);
+    print_set(&s);
+    return 0;
 }
